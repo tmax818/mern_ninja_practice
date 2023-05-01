edc0fbd473754cdcff25025bb96df4035db58dda
tmax818 <tylermaxwell661@gmail.com>
2023-05-01 12:24:04 -0700

    add todos

---
+++ ninja.js
@@ -0,0 +1,15 @@
+// TODO Create a Ninja class
+
+// TODO add an attribute: name
+
+// TODO add an attribute: health
+
+// TODO add a attribute: speed - give a default value of 3
+
+// TODO add a attribute: strength - give a default value of 3
+
+// TODO add a method: sayName() - This should log that Ninja's name to the console
+
+// TODO add a method: showStats() - This should show the Ninja's name, strength, speed, and health.
+
+// TODO add a method: drinkSake() - This should add +10 Health to the Ninja
